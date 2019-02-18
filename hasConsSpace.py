882fb91eddb1c6202c25990fea6ab05ef686178e
x8a2b <x8a2b@cs.ubc.ca>
2019-02-17 19:39:38 -0800

    hi

---
+++ hasConsSpace.py
@@ -0,0 +1,25 @@
+def isThisASpace(str, index):
+	if (str[index] == " "):
+		return true
+	else:
+		return false
+
+def hasConsSpaces(str, index):
+	if (isThisASpace(str, index) and isThisASpace(str, index+1)):
+		return true
+	else:
+		false
+
+def howManySpaces(str, index):
+	n = 2
+	if (isThisASpace(str, index+n)):
+		while(isThisASpace(str,index+n)):
+			n = n + 1
+	return n
+
+testString = "     " #5 spaces
+n = len(testString)
+
+for i in range(0,n):
+	if (hasConsSpaces(testString, i)):
+		print(howManySpaces(testString,i))
