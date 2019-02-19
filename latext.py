bc79d9928d8b259121aa9139c9de0eaca96098f3
isthisevenreal <rithvik.al@gmail.com>
2019-02-18 17:48:13 -0800

    Update

--- latext.py
+++ latext.py
@@ -1,14 +1,22 @@
+import sys
+import time
+
 filename1 = "test.txt"
 def main(filename):
+    # This is without splitting lines
+    # s = open(filename).read()
+    # s = s.replace('\t', '*')
+    # print(s)
+
+    # This is with splittin lines
     file = open(filename)
     string = file.read().splitlines()
+    print(string)
+    for i in range(0, len(string)):
+        string[i] = string[i].replace("\t", "*")
+        print(string[i])
     print(string)
 
 
 
 main(filename1)
-#comment
-
-#commment1
-#c
-#please
