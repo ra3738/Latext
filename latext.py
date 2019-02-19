72a3e1332afb1cd098827b0311ff4254e4fd4fdb
isthisevenreal <rithvik.al@gmail.com>
2019-02-18 19:03:22 -0800

    Changes to Main

--- latext.py
+++ latext.py
@@ -4,18 +4,18 @@ import time
 filename1 = "test.txt"
 def main(filename):
     # This is without splitting lines
-    # s = open(filename).read()
-    # s = s.replace('\t', '*')
-    # print(s)
+    s = open(filename).read()
+    s = s.replace('\n', '*')
+    print(s)
 
-    # This is with splittin lines
-    file = open(filename)
-    string = file.read().splitlines()
-    print(string)
-    for i in range(0, len(string)):
-        string[i] = string[i].replace("\t", "*")
-        print(string[i])
-    print(string)
+    # This is with splitting lines
+    # file = open(filename)
+    # string = file.read().splitlines()
+    # print(string)
+    # for i in range(0, len(string)):
+    #     string[i] = string[i].replace("\t", "*")
+    #     print(string[i])
+    # print(string)
 
 
 
