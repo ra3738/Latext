d55803b6616d4971ed2eb4506cdd795c155cf4d3
hardik-ddod <hardik.singhi@alumni.ubc.ca>
2019-02-18 20:45:26 -0800

    Fixed bugs and added replaceSpaces()

--- hasConsSpace.py
+++ hasConsSpace.py
@@ -1,27 +1,56 @@
+#Checks if current index is a space, returns boolean
 def isThisASpace(str, index):
 	if (str[index] == " "):
-		return true
+		return True
 	else:
-		return false
+		return False
+
+#Checks if current index and next index are spaces, returns boolean
+#If at second last index, returns false
 
 def hasConsSpaces(str, index):
-	if (isThisASpace(str, index) and isThisASpace(str, index+1)):
-		return true
+	if (index + 1 < len(str)):
+		if (isThisASpace(str, index) and isThisASpace(str, index+1)):
+			return True
+		else:
+			return False
 	else:
-		false
+		return False
 
+#Counts no. of consecutive spaces, returns int
 def howManySpaces(str, index):
-	n = 2
-	if (isThisASpace(str, index+n)):
-		while(isThisASpace(str,index+n)):
+	length = len(str)
+	index = index
+	n = 0
+	while (index < length):
+		if (isThisASpace(str,index)):
 			n = n + 1
-	return n
+			index = index + 1
+		else:
+			return n
+	return n 
 
-testString = "     " #5 spaces
-n = len(testString)
+#replaces spaces number of indices after and including index with strToReplaceWith
+#Returns string
+def replaceSpaces(strToReplace, index, strToReplaceWith, spaces):
+	strList = list(strToReplace)
+	while (not(spaces== 0)):
+		strList[index] = strToReplaceWith
+		index = index + 1
+		spaces = spaces - 1
+	return "".join(strList)
 
-for i in range(0,n):
+testString = "  m  m " 
+
+i = 0
+while (i < len(testString)):
 	if (hasConsSpaces(testString, i)):
-		print(howManySpaces(testString,i))
+		j = howManySpaces(testString, i)
+		print(j)
+		testString = replaceSpaces(testString, i, "", j)
+		i = i + j - 1
+	else:
+		i = i + 1
 
+print(testString)
 #comment
